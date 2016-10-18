d87b8a6d11d12416757cd37f2762a2fda9fb375e
Eduardo Alonso <alonsoeduardo@gmail.com>
2016-10-18 15:21:10 -0300

    Enlace Formulario con Componente

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Mi Primer Angular 2 </h1>'
+  templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  onSubmit(value : any){
+    console.log(value);
+  }
+ }
