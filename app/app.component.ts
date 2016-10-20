362dc9f76c5b2072e3d61d56c304703b13c9ade2
Eduardo Alonso <alonsoeduardo@gmail.com>
2016-10-19 22:41:51 -0300

    Formulario con columnas

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  templateUrl: './app/app.component.html'
+  templateUrl: './app/app.component.html',
+  styles: [`input.ng-invalid{border-left:5px solid red;}
+            input.ng-valid{border-left:5px solid green;}`]
 })
 export class AppComponent {
   onSubmit(value : any){
